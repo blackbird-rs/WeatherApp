4e8ca44046bddd38c0a8ed716d6c977e7a2f9f0a
Petar <petarkovrlija1995@gmail.com>
2024-10-24 21:25:10 +0200

    Added place selection + get requests for current weather and forecast
    I've changed the place search to include buttons, which on click get the api response for the current weather for the picked city, and start an interval in which the current weather information will be refreshed. I've also added a select field that triggers forecast request on change and logs the results

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 const API_KEY = 'aa2e5ec578a180c792872490febac252'
 const GEOCODING_URL = 'http://api.openweathermap.org/geo/1.0/direct?q='
+const CURRENT_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?lat='
+const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat='
 
+var selectedLat = ''
+var selectedLon = ''
+
+//Dom section
 var placeInputField = document.getElementById('placeInput');
-placeInputField.addEventListener('input', function() {
+placeInputField.addEventListener('input', function(){
   placeSearch(placeInputField.value);
 });
 
+var daySelection = document.getElementById('daySelection');
+daySelection.addEventListener('change', getForecast);
+
+var resultsWrapper = document.getElementById('placeResults');
+
+//Places search section
 function placeSearch(value) {
   if(value != ""){
     axios.get(GEOCODING_URL + value + '&limit=5&appid=' + API_KEY)
@@ -22,7 +34,6 @@ function placeSearch(value) {
 }
 
 function updatePlaces(data) {
-  var resultsWrapper = document.getElementById('placeResults');
   if (resultsWrapper) {
     while(resultsWrapper.firstChild){
       resultsWrapper.removeChild(resultsWrapper.firstChild);
@@ -30,12 +41,56 @@ function updatePlaces(data) {
     if(data != null){
       for (var i = 0; i < data.length; i++) {
         const li = document.createElement('li');
-        li.textContent = data[i].name + ", " + data[i].country + ", " + data[i].lat + ", " + data[i].lon;
+        const button = document.createElement('button');
+        button.id = 'placeResult' + i;
+        button.setAttribute('data-lat', data[i].lat);
+        button.setAttribute('data-lon', data[i].lon);
+        button.onclick = function(){
+          pickPlace(this);
+        };
+        button.innerText = data[i].name + ", " + data[i].country;
+        li.appendChild(button);
         resultsWrapper.appendChild(li);
-      }
+      }    
     }
   } 
   else {
     console.error(`No element found with id: placeResults`);
   }
 }
+
+function pickPlace(button){
+  placeInputField.value = button.innerText;
+  selectedLat = button.getAttribute('data-lat');
+  selectedLon = button.getAttribute('data-lon');
+  while(resultsWrapper.firstChild){
+    resultsWrapper.removeChild(resultsWrapper.firstChild);
+  }
+  getCurrentWeather();
+  setInterval(getCurrentWeather, 600000);
+}
+
+//Current weather section
+function getCurrentWeather(){
+  console.log('updated weather');
+  axios.get(CURRENT_WEATHER_URL + selectedLat + '&lon=' + selectedLon + '&appid=' + API_KEY)
+      .then(response => {
+        console.log(response.data);
+        })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        });
+}
+
+//Forecast section
+function getForecast(){
+  var numberOfDays = daySelection.value;
+  axios.get(FORECAST_URL + selectedLat + '&lon=' + selectedLon + '&cnt=' + numberOfDays + '&appid=' + API_KEY)
+      .then(response => {
+        console.log(response.data);
+        })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        });  
+}
+
